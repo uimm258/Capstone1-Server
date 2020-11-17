b43c36b0cfd140c34dedb08c4eec788736d9d766
uimm258 <uimm258@gmail.com>
2020-11-17 01:16:36 -0800

    ..

--- src/config.js
+++ src/config.js
@@ -3,8 +3,7 @@ require('dotenv').config();
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: (process.env.NODE_ENV === 'test')
-    ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://yrspace:testpw@localhost/category',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://yrspace:testpw@localhost/category-test',
     API_TOKEN: process.env.API_TOKEN || '143891f9-9ace-49d9-9a4a-5123d3809ff3',
 }
--- src/server.js
+++ src/server.js
@@ -2,7 +2,10 @@ const app = require('./app')
 const knex = require('knex')
 const { PORT, DATABASE_URL } = require('./config')
 
-const db = knex({ client: 'pg', connection: DATABASE_URL, })
+const db = knex({ 
+  client: 'pg', 
+  connection: DATABASE_URL, 
+})
 
 app.set('db', db)
 
